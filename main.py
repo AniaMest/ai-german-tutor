81197e69c2849cb36bc9af89e922f078e8c63e8a
anika-mestvirishvili-4 <anikamestvirishvili4@gmail.com>
2026-03-10 01:57:47 +0400

    Options to continue or stop the program added
    A while loop was used

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 import json
 import random
 
-with open("data/connectors.json") as file:
-    exercises = json.load(file)
+while True:
 
-exercise1 = random.choice(exercises)
-options = exercise1["options"]
+    with open("data/connectors.json") as file:
+        exercises = json.load(file)
 
-def answer_options():
-    result = ""
-    for n in range(len(options)):
-        result += str(n+1) + ". "+ options[n] + "\n"
-    return result
+    exercise1 = random.choice(exercises)
+    options = exercise1["options"]
 
-print(exercise1["question"])
-print(answer_options())
+    def answer_options():
+        result = ""
+        for n in range(len(options)):
+            result += str(n+1) + ". "+ options[n] + "\n"
+        return result
 
-user_answer = options[int(input("Your answer: "))-1]
+    print(exercise1["question"])
+    print(answer_options())
+    m = int(input("Your answer: "))
+    user_answer = options[m-1]
 
-def answer_checker():
-    if user_answer == exercise1["answer"]:
-        return "Correct!"
-    return "Incorrect.\nCorrect answer: "+exercise1["answer"]+"\n"+exercise1["explanation"] 
+    def answer_checker():
+        if user_answer == exercise1["answer"]:
+            return "Correct!"
+        return "Incorrect.\nCorrect answer: "+exercise1["answer"]+"\n"+exercise1["explanation"]
 
-print(answer_checker())
+    print(answer_checker())
+
+    response = input("|Press Enter for next task|\n|or type q to quit|")
+
+    if response == "q":
+        break
+    continue
 
